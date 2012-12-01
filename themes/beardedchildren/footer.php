a54a838aaee451feb8a1d441c8795cf1723a03e8
Rusty <rustin.l.odom@gmail.com>
2012-12-01 09:04:36 -0700

    Added Coming Soon page plugin, added footer, changed name of coming soon page to coming-soon.php

---
+++ themes/beardedchildren/footer.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<p style='background: #CCCCCC;'>This is the footer for Bearded Children. Ew! Hairy feet!</p>
--- themes/beardedchildren/index.php
+++ themes/beardedchildren/index.php
@@ -7,3 +7,26 @@
  */
 
 ?>
+
+<?php get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php the_title(); ?><br/>
+                                        <?php the_content(); ?>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
