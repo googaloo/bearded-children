69a54fd679e3cba57e277330af74223902e44fa3
Rusty <rustin.l.odom@gmail.com>
2012-12-02 17:02:25 -0700

    Created main part of header.php including search

---
+++ themes/beardedchildren/header.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title></title>
+<link type='text/css' rel='stylesheet' href='<?php bloginfo('template_directory'); ?>/style.css' />
+
+<?php wp_head(); ?>
+</head>
+<body>
+    
+    <div class='social-box'>
+        
+        <a href='#' class='social-link social-yt'>Youtube</a>
+        <a href='#' class='social-link social-fb'>Facebook</a>
+        <a href='#' class='social-link social-tw'>Twitter</a>
+        <a href='#' class='social-link social-rd'>Reddit</a>
+        <a href='#' class='social-link social-em'>Email</a>
+        
+    </div>
+    <div class='main-container'>
+        
+        <div class='masthead'>
+        
+            <div class='logo-box'></div>
+            <nav class='main-nav'>
+                <?php $li_pages_args = array(
+                  
+                    'title_li' => ''
+                    
+                );
+                
+                ?>
+                <?php wp_list_pages($li_pages_args); ?>
+
+            </nav>
+            <div class='search-bar-box'>
+                
+                <?php include('searchform.php'); ?>
+                
+            </div><!-- end search-bar-box -->
+            
+        </div><!-- end masthead -->
---
+++ themes/beardedchildren/searchform.php
@@ -0,0 +1,8 @@
+<form method=”get” id=”searchform” action=”<?php bloginfo('template_directory'); ?>/index.php”>
+    
+    <div>
+        <input type=”text” size=”18″ name=”s” id=”s” />
+        <input type="image" src="<?php bloginfo('template_directory'); ?>/images/home/search-icon.png" id="searchsubmit" value="Search" class='search-btn'/>
+    </div>
+    
+</form>
