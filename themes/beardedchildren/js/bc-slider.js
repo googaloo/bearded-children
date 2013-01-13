2f7e57857202623a24d0ea830df0e7620d56825c
Rusty <rustin.l.odom@gmail.com>
2013-01-13 00:14:58 -0700

    Reimporting BC database / images and work on slider creating infinite loop for "previous" button

--- themes/beardedchildren/js/bc-slider.js
+++ themes/beardedchildren/js/bc-slider.js
@@ -8,33 +8,43 @@ jQuery(document).ready(function($) {
 	var currentSlide = 1;
 	// Count how many slides exist
 	var numSlides = $('.slides-list > li').length;
-	// Grab first slide HTML
-	var firstSlide = '<li>'+$('.slides-list li').filter(':first').html()+'</li>';
-	// Grab last slide HTML
-	var lastSlide = '<li>'+$('.slides-list li').filter(':last').html()+'</li>';
-	
-	// Set up duplicates on startup
-	// After Last
-	$('.slides-list').append(firstSlide);
-	// Before First
-	$('.slides-list').prepend(lastSlide);
+	// Grab HTML for all slides
+	var slidesHTML = $('.slides-list').html();	
 	
 	// Timer for when slide is not engaged
-	
+
 	// Handle Navigation Buttons
+	// Handle Previous Button
 	$('.bc-slider-nav-prev').click( function() {
-		
+
+		// Handle creating and prepending slides before current for infinite scrolling
+		// through slides
 		if(currentSlide == 1) {
+			currentSlide = numSlides;
+			// reset the slides to appear before current slide
+			$('.slides-list').prepend(slidesHTML);
+			// reset the position of the current slide
+			var currentPos = -890 * numSlides;
+
+			$('.slides-list').css("left", currentPos+'px');
 			
-		}
-		
-		$('ul.slides-list').animate({
+			$('ul.slides-list').animate({
 			left: '+=890' },
 			500
-		);
+			);
+
+		} else {
+			currentSlide--;
+			$('ul.slides-list').animate({
+			left: '+=890' },
+			500
+			);
+		}
+		
 
 	});
 	
+	// Handle Next Button
 	$('.bc-slider-nav-next').click( function() {
 		
 		$('ul.slides-list').animate({
