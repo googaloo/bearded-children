9b698c5ac8856e2aa36cefd0644d2803e3bfdcc4
Rusty <rustin.l.odom@gmail.com>
2013-03-18 08:18:13 -0700

    cleanup // BC Video Archive basic logic complete
    still need to structure

--- themes/beardedchildren/bc-video-archive.php
+++ themes/beardedchildren/bc-video-archive.php
@@ -7,40 +7,6 @@
 <?php get_header(); ?>
 
 <h1>Channels</h1>
-<<<<<<< HEAD
-
-<div class='channel_container'>
-	
-<?php
-	
-	$channel_args = array( 'post_type' => 'bc-channel', 'posts_per_page' => 10 );
-	$channel_loop = new WP_Query( $channel_args );
-	
-	if ( $channel_loop->have_posts() ) : ?>
-		
-		<?php while ( $channel_loop->have_posts() ) : $channel_loop->the_post(); ?>
-	
-	<div class='channel-box channel-<?php echo get_the_id(); ?>'>
-	
-	
-	
-	</div>
-
-		<?php endwhile; ?> 
-	
-	<?php endif; ?>
-
-	
-	    
-</div><!-- end .channel_container -->
-
-
-<?php
-
-$args = array( 'post_type' => 'bc-videos', 'posts_per_page' => 10 );
-$loop = new WP_Query( $args );
-=======
->>>>>>> Fixing missing repo?
 
 <div class='channel_container'>
 	
@@ -49,34 +15,15 @@ $loop = new WP_Query( $args );
 	$args = array('post_type' => 'bc-channel');
 	$channel = get_posts($args);
 
-<<<<<<< HEAD
-		<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-		
-		<?php 
-		
-		$meta_channel = get_post_meta(get_the_id(), 'wpcf-channel', true); 
-		$meta_descr = get_post_meta(get_the_id(), 'wpcf-description', true); 
-	
-		?>
-		
-		<p>Channel: <?php echo $meta_channel; ?></p>
-		<p>Description: <?php echo $meta_descr; ?></p>
-		
-	<?php endwhile;
-=======
 	for($i=1; $i<=$num_channels; $i++): ?>
 
 		<h2><?php echo $channel[$i-1]->post_title; ?></h2>
 		<div><?php echo get_the_post_thumbnail($channel[$i-1]->ID, 'thumbnail'); ?></div>
->>>>>>> Fixing missing repo?
 
 		<?php
 		$skits_loop = new WP_Query(array('post_type'=>'bc-videos'));
 		if($skits_loop->have_posts()) :
 
-<<<<<<< HEAD
-<?php wp_reset_query(); ?>
-=======
 			while($skits_loop->have_posts()) : $skits_loop->the_post(); ?>
 
 				<?php $channel_type = get_post_meta(get_the_id(), 'wpcf-channel', true) ?>
@@ -88,7 +35,6 @@ $loop = new WP_Query( $args );
 			<?php endwhile; ?>
 
 		<?php endif; ?>
->>>>>>> Fixing missing repo?
 
 	<?php endfor; ?>
 		
