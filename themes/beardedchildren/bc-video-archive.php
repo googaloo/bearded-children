57d62bad2f7b56cf2877c7d229b57317522eb56b
Rusty <rustin.l.odom@gmail.com>
2013-03-21 08:50:36 -0700

    Fixed displaying images for BC-Video Archive
    Friggin "attached" images... Had to install Unattach plugin

--- themes/beardedchildren/bc-video-archive.php
+++ themes/beardedchildren/bc-video-archive.php
@@ -11,22 +11,22 @@
 <div class='channel_container'>
 	
 	<?php
-	$num_channels=wp_count_posts('bc-channel')->publish;
+	$num_channels = wp_count_posts('bc-channel')->publish;
 	$args = array('post_type' => 'bc-channel', 'orderby' => 'menu_order', 'order' => 'ASC');
-	$channel = get_posts($args);
+	$channel = get_posts($args); 
 
-	for($i=1; $i<=$num_channels; $i++): 
-
-		$images = get_children( array( 'post_parent' => $channel[$i-1]->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
+	for($i=0; $i<=$num_channels-1; $i++) :
+		print_r($channel[$i]->ID);
+		$images = get_children( array( 'post_parent' => $channel[$i]->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
 		if ( $images ) :
 			$image = array_shift( $images );
 			$channel_bg = wp_get_attachment_url( $image->ID );
 		endif; ?>
 
-		<h2><?php echo $channel[$i-1]->post_title; ?></h2>
+		<h2><?php echo $channel[$i]->post_title; ?></h2> <?php //print_r($image); ?>
 		<div class='channel-box'>
-			<div class='channel-logo'><?php echo get_the_post_thumbnail($channel[$i-1]->ID, 'large'); ?></div>
-			<div class='channel-img' style='background: url(<?php echo $channel_bg; ?>);'>This is where the cheese is made.</div><!-- end .channel-img <?php echo $i-1; ?> -->
+			<div class='channel-logo'><?php echo get_the_post_thumbnail($channel[$i]->ID, 'large'); ?></div>
+			<div class='channel-img' style='background: url(<?php echo $channel_bg; ?>);'>This is where the cheese is made.</div><!-- end .channel-img <?php echo $i; ?> -->
 		</div><!-- end .channel-box -->
 
 		<?php
